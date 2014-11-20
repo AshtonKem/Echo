e3af7571ec4f70ef426426fe576dc1cee902c5bf
Ashton Kemerling <ashtonkemerling@gmail.com>
2014-11-20 14:26:48 -0500

    Better name and docs.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject echo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Record test Ajax requests for documentation usage"
+  :url "http://github.com/AshtonKem/echo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
